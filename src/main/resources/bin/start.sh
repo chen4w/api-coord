78c12b4adbc2159685615efa9fefdeaa54933d63
lhc <taixingyiji@126.com>
2021-11-16 11:41:46 +0800

    <fix> 优化启动及停止脚本，将sql日志级别改为debug

--- src/main/resources/bin/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-PWD="$( cd "$( dirname "$0"  )" && pwd  )"
-cd "$PWD"
-nohup java  -jar ../lib/Interface-Cooperation-Middleware-1.0-SNAPSHOT.jar >/dev/null 2>&1 &
-echo $! > $PWD'/process.pid'
-echo "Middleware is starting..."
