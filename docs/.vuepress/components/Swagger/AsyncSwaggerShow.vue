03b96e13ca9e3a0a643735cf4e8d567c94effdf7
lhc <taixingyiji@126.com>
2021-11-29 09:16:25 +0800

    <doc> 新增文件缓存说明，修改异步接口描述

---
+++ docs/.vuepress/components/Swagger/AsyncSwaggerShow.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="importSwagger" style="width: 80%;margin: 10px auto;text-align: center;">
+    <div id="swagger-ui-async" >
+    </div>
+  </div>
+</template>
+<script>
+import { SwaggerUIBundle,SwaggerUIStandalonePreset } from 'swagger-ui-dist'
+export default {
+  data() {
+    return {
+    }
+  },
+  created() {
+  },
+  mounted() {
+    const ui = SwaggerUIBundle({
+      url: "https://btajl.gitee.io/api-coord/swagger/async.json",
+      // url: "/swagger/async.json",
+      dom_id: '#swagger-ui-async',
+      presets: [
+        SwaggerUIBundle.presets.apis,
+        SwaggerUIStandalonePreset
+      ],
+      layout: "BaseLayout"
+    })
+  }
+}
+</script>
+<style scoped>
+@import "~swagger-ui-dist/swagger-ui.css";
+</style>
